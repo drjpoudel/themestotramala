6a1f5cf5ddb1c159cda007f8183bfcdd033c49a4
drjpoudel <drjpoudel@gmail.com>
2025-08-28 11:15:13 +0545

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+    </div><!-- #content -->
+
+    <footer id="colophon" class="site-footer" role="contentinfo">
+        <div class="site-info">
+            &copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?>. All Rights Reserved.
+        </div><!-- .site-info -->
+    </footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
